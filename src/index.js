ad930aa79423b007388354ca8bdd6763910dc75a
Andrea Martinelli <andrea.martinelli@rai.it>
2020-01-27 12:18:16 +0100

    Init

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import "./example-manager/example.manager.ts";
---
+++ webpack.config.dev.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'output')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        }
+      }
+    ]
+  }
+}
